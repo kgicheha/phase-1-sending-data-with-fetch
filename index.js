e021af97ec46313f05c492c6b2b643dbc7412bfe
Kevin Gicheha <gichehakevin@gmail.com>
2022-06-13 08:45:54 -0700

    commit tree 4f5b0aecce45a6fa8a12d98d22c85ed990be3e92

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const { bodyParser } = require("json-server");
+
 // Add your code here
+function submitData (name, email) {
+    return fetch( 'http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+            name,
+            email
+        } )
+    } )
+    .then( response =>response.json())
+    .then( object => document.body.innerHTML = object['id']
+    )
+    .catch(error => document.body.innerHTML = error.message
+    )
+
+}
